f4d7f4dab18853c63f598db3558f68bfb4bce382
Jinn Tsang <jinntsang@192-168-1-101.tpgi.com.au>
2021-10-15 16:00:50 +1100

    updated functionality, able to translate more to english

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morseAlpha = {
     b: "-..",
     c: "-.-.",
     d: "-..",
+    h: "....",
     e: ".",
     f: "..-.",
     g: "--.",
@@ -25,9 +26,13 @@ const morseAlpha = {
     x: "-..-",
     y: "-.--",
     z: "--..",
-    "/": "/",
-    " ": " ",
+    "/": " ",
+    " ": "/",
 };
+const alphaMorse = Object.entries(morseAlpha).reduce((acc, [key, value]) => {
+    acc[value] = key;
+    return acc;
+}, {});
 
 console.log(morseAlpha);
 
@@ -40,25 +45,42 @@ translateBtn.addEventListener("click", (event) => {
     // grabbing the input value and storing in textInput
     let textInput = input.value.toLowerCase();
 
-    // const test = textInput.slice(0, 5);
-    // console.log(test);
-    if (textInput.slice(0, 4).includes("-", ".", "/", " ")) {
-        const morseArr = textInput.split(" ");
+    // add an alert for an empty string
+
+    console.log(/[./ -]/.test(textInput));
+    // (/[.-/]/.test(textInput))
+    // textInput.slice(0, 4).includes(".", "-", "/", " ")
+    if (/[./-]/.test(textInput)) {
+        const morseArr = textInput.split("/"); //split words with a slash /
         console.log("splitting morse words", morseArr);
-        //need to find a way to replace / with an empty string, maybe regex??
+
+        // map again for individual characters ("")
+        const morse = morseArr.map((letter) => {
+            return letter.split(" ");
+        });
+        console.log(morse);
 
         // matching our morse values with morse values in our object.
         // then outputting the key (which is our english letter)
-        const toEnglish = morseArr.map((letter) => {
-            Object.values(morseAlpha).includes(letter);
-            return Object.keys(morseAlpha).find(
-                (key) => morseAlpha[key] === letter
-            );
+        // const toEnglish = morse.map((letter) => {
+        //     Object.values(morseAlpha).includes(letter);
+        //     return Object.keys(morseAlpha).find(
+        //         (key) => morseAlpha[key] === letter
+        //     );
+        // });
+        // console.log(toEnglish);
+
+        const toEnglish = morse.map((word) => {
+            return word
+                .map((letter) => {
+                    return alphaMorse[letter];
+                })
+                .join("");
         });
-        console.log(toEnglish);
+        console.log("joining characters", toEnglish);
 
         // converting our english array to a string
-        const engToString = toEnglish.join("");
+        const engToString = toEnglish.join(" ");
         console.log(engToString);
 
         output.innerHTML = engToString;
@@ -75,13 +97,13 @@ translateBtn.addEventListener("click", (event) => {
         console.log(toMorse);
 
         // converting morse array to string
-        const morseToString = toMorse.join("");
+        const morseToString = toMorse.join(" ");
         console.log(morseToString);
 
         //morse to output display
         output.innerHTML = morseToString;
 
-        // .... . .-.. .-.. --- / -- -.-- / -. .- -- . / .. ... / .--- .. -. -.
+        //not currently spacing between words // push array to empty string?
     }
 });
 
